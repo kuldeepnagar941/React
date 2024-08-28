676b8f20916e73c88550b3e29117cf057f80f45f
kuldeepnagar941 <kuldeepnagar941@gmail.com>
2024-08-28 12:20:37 +0530

    new

--- myproj/src/Header.jsx
+++ myproj/src/Header.jsx
@@ -1,6 +1,6 @@
 function Header(){
     return(
-       <div className="h1"> <h1 id="h">hello</h1></div>
+       <div className="h1"> <h1 id="h">hello kuldeep nagar</h1></div>
     )
 }
 export default Header
